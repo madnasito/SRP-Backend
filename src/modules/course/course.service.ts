e164f43b44d38c46a218c276ef58c4ef1ffacf4a
MADNA <madnamv@gmail.com>
2026-01-16 23:32:06 -0400

    add: header

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -47,6 +47,7 @@ export class CourseService {
                 {
                     headers: {
                         ...form.getHeaders(),
+                        'Content-Type': 'multipart/form-data',
                     },
                 },
             );
